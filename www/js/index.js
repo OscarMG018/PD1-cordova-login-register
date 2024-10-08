66c10ef7732d86f45250940c9d1a264431f301aa
OscarMG018 <108469203+OscarMG018@users.noreply.github.com>
2024-10-08 17:05:21 +0200

    entrega

--- www/js/index.js
+++ www/js/index.js
@@ -19,11 +19,72 @@
 
 // Wait for the deviceready event before using any of Cordova's device APIs.
 // See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
-document.addEventListener('deviceready', onDeviceReady, false);
 
-function onDeviceReady() {
-    // Cordova is now initialized. Have fun!
+let loginForm = document.getElementById('loginForm');
+let registerForm = document.getElementById('registerForm');
 
-    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-    document.getElementById('deviceready').classList.add('ready');
+let loginButton = document.getElementById('loginButton');
+let registerButton = document.getElementById('registerButton');
+
+let users = [];
+
+function login() {
+    let username = document.getElementById('usuari').value;
+    let password = document.getElementById('contrasenya').value;
+    let user = checkUser(username, password);
+    console.log(user);
+    console.log(username);
+    console.log(password);
+    console.log(users);
+    if (user !== undefined) {
+        alert('Login exitós');
+    } else {
+        alert('Login incorrecte');
+    }
 }
+
+function register() {
+    let username = document.getElementById('reg_usuari').value;
+    let password = document.getElementById('reg_contrasenya_1').value;
+    let passwordConfirm = document.getElementById('reg_contrasenya_2').value;
+    let email = document.getElementById('reg_email').value;
+    if (password !== passwordConfirm) {
+        alert('Error en les contrasenyes');
+    } else if (checkDuplicateUser(username,email)) {
+        alert('El nom o l\'email ja existeix a la base de dades');
+    } else {
+        addUser(username, password, email);
+        alert('Nou usuari registrat correctament');
+        console.log(users);
+    }
+    username = '';
+    password = '';
+    passwordConfirm = '';
+    email = '';
+}
+
+function checkUser(username, password) {
+    return users.find(user => user.username === username && user.password === password);
+}
+
+function checkDuplicateUser(username,email) {
+    return users.some(user => user.username === username || user.email === email);
+}
+
+function addUser(username, password, email) {
+    users.push({ username, password, email });
+}
+
+function initUsers() {
+    users.push({ username: 'pepe', password: 'pepe123', email: 'pepe@example.com' });
+    users.push({ username: 'lola', password: 'lola123', email: 'lola@example.com' });
+    users.push({ username: 'ieti', password: 'cordova', email: 'ieti@example.com' });
+}
+
+function main() {
+    initUsers();
+    loginButton.addEventListener('click', login);
+    registerButton.addEventListener('click', register);
+}
+
+document.addEventListener('DOMContentLoaded', main);
